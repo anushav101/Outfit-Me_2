0c8960aaa3b6ac26085bfe42b8a31139add10434
anushav101 <anushav101@gmail.com>
2016-08-04 13:43:18 -0700

    MODAL VIEW! thank you  randy <3

--- Outfit Me_2/ViewController.swift
+++ Outfit Me_2/ViewController.swift
@@ -12,13 +12,43 @@ import Parse
 class ViewController: UIViewController {
     var categoryDataProvider = [TopsDataProvider.sharedInstance, BottomsDataProvider.sharedInstance, OuterwearDataProvider.sharedInstance, DressesDataProvider.sharedInstance, AccessoriesDataProvider.sharedInstance, ShoesDataProvider.sharedInstance]
     
+    @IBOutlet weak var modalView: UIView!
+    @IBOutlet weak var gotitButton: UIButton!
+    @IBOutlet weak var messageLabel: UILabel!
+    @IBOutlet weak var cancelButton: UIButton!
+
     
-    @IBOutlet weak var tableView: UITableView!
-    var photoTakingHelper: PhotoTakingHelper!
     
-    
-    @IBAction func deleteClothing(sender: AnyObject) {
+    @IBAction func cancelPressed(sender: UIButton) {
+        print("HI")
         
+        UIView.animateWithDuration(0.5, delay: 0.0, options: UIViewAnimationOptions.CurveEaseOut, animations: {
+            () -> Void in
+            self.modalView.alpha = 0
+            self.modalView.hidden = false
+            
+        }) { (completed) in
+            
+        }
+
+    }
+    
+    
+    
+    @IBAction func gotitPressed(sender: UIButton) {
+        print("HI")
+        
+        UIView.animateWithDuration(0.5, delay: 0.0, options: UIViewAnimationOptions.CurveEaseOut, animations: {
+            () -> Void in
+            self.modalView.alpha = 0
+            self.modalView.hidden = false
+            
+        }) { (completed) in
+            
+        }
+
+    
+        // delete code here
         for object in objectsToDelete {
             let query = PFQuery(className: "Product")
             query.whereKey("objectId", equalTo: (object.objectId)!)
@@ -30,6 +60,8 @@ class ViewController: UIViewController {
             }
             
         }
+        
+        objectsToDelete = []
         
         let triggerTime = (Int64(NSEC_PER_SEC) * 1)
         dispatch_after(dispatch_time(DISPATCH_TIME_NOW, triggerTime), dispatch_get_main_queue(), { () -> Void in
@@ -81,8 +113,40 @@ class ViewController: UIViewController {
                 }
             }
             
-
+            
         })
+        
+    }
+    @IBOutlet weak var tableView: UITableView!
+    var photoTakingHelper: PhotoTakingHelper!
+    
+    
+    
+    @IBAction func deleteClothing(sender: AnyObject) {
+        
+        if (objectsToDelete.count > 1) {
+            messageLabel.text = "You are about to delete \(objectsToDelete.count) items"
+        }
+        else{
+            messageLabel.text = "You are about to delete \(objectsToDelete.count) item"
+        }
+        
+        if (objectsToDelete.count == 0) {
+            return
+        }
+        else {
+            UIView.animateWithDuration(0.5, delay: 0.0, options: UIViewAnimationOptions.CurveEaseIn, animations: {
+                () -> Void in
+                self.modalView.alpha = 1
+                self.modalView.hidden = false
+                
+            }) { (completed) in
+                
+            }
+        
+        }
+    
+  
         
         
         
@@ -94,12 +158,40 @@ class ViewController: UIViewController {
         super.viewDidLoad()
         let parsePhoto = ParsePhotos()
         // Do any additional setup after loading the view, typically from a nib.
+//        self.modalView.frame.origin = CGPoint(x: 175,y: 603)
+        self.modalView.hidden = true
+        self.modalView.layer.zPosition = 5
+        gotitButton.layer.zPosition = 5
+        cancelButton.layer.zPosition = 5
+        messageLabel.layer.zPosition = 5
+        // Customize the modal view container
+        modalView.backgroundColor = UIColor.whiteColor()
+        modalView.layer.cornerRadius = 15
+        modalView.alpha = 0
         
+        // Customize the button in the modal view
+        gotitButton.layer.cornerRadius = 10
+        gotitButton.setTitleColor(UIColor.whiteColor(), forState: .Normal)
+        gotitButton.backgroundColor = UIColor(colorLiteralRed: 0/255, green: 153/255, blue: 255/255, alpha: 0.75)
+        
+        // Customize the message label on the modal
+       
+        messageLabel.textColor = UIColor.darkGrayColor()
+        
+        cancelButton.layer.cornerRadius = 10
+        cancelButton.setTitleColor(UIColor.whiteColor(), forState: .Normal)
+        cancelButton.backgroundColor = UIColor(colorLiteralRed: 0/255, green: 153/255, blue: 255/255, alpha: 0.75)
+
         
     }
     
+   
+    
     override func viewWillAppear(animated: Bool) {
+        
         super.viewWillAppear(animated)
+        
+               
         
         TopsDataProvider.sharedInstance.getAllClothing { (success: Bool) in
             if success {
